8c6af6782198cf62929d733385703e680f281a36
caelwinner <adolfoecs@gmail.com>
2018-04-16 19:48:29 +0100

    wip1

---
+++ src/main/java/uk/co/caeldev/invoicer/auth/Application.java
@@ -0,0 +1,15 @@
+package uk.co.caeldev.invoicer.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import uk.co.caeldev.base.auth2.config.EnableBaseAuthServer;
+
+@SpringBootApplication
+@EnableBaseAuthServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/uk/co/caeldev/invoicer/auth/ApplicationTest.java
@@ -0,0 +1,25 @@
+package uk.co.caeldev.invoicer.auth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = { Application.class })
+@ActiveProfiles("test")
+public class ApplicationTest {
+
+    @Test
+    public void should() throws Exception {
+        //Given
+         
+        //When
+        
+         
+        //Then
+    }
+}
